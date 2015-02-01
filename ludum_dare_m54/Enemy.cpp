47fdecac56bd4dbc7f178f7f4b10d3c9cd50c866
Super Heavy <hd2319@gmail.com>
2015-02-01 01:53:03 -0800

    adding stubs for update functions

--- ludum_dare_m54/Enemy.cpp
+++ ludum_dare_m54/Enemy.cpp
@@ -9,3 +9,10 @@ Enemy::Enemy(void)
 Enemy::~Enemy(void)
 {
 }
+
+
+void Enemy::Update(float dt)
+{
+    //OMG!
+    return;
+}
--- ludum_dare_m54/Tile.cpp
+++ ludum_dare_m54/Tile.cpp
@@ -8,3 +8,9 @@ Tile::Tile(void)
 Tile::~Tile(void)
 {
 }
+
+void Tile::Update(float dt)
+{
+    //LOL!
+    return;
+}
